11e99c37f2e55e5512fd3121fafa986bbf3aaefa
Nikunj Gupta <nikunj.nkgpta@gmail.com>
2020-08-08 07:02:58 +0530

    Adds bspwm config files

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-name=nk
-pkill polybar
-if type "xrandr"; then
-  for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=${monitor} polybar --reload ${name}&
-  done
-else
-  polybar --reload ${name} &
-fi
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar default -c ~/.config/polybar/config &
